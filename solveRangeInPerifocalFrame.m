d1d16a6778fc594b2839b445d4b47eb71c5f7d9c
CMH <CMH@DESKTOP-UM53FOC>
2023-05-26 14:58:17 +0900

    20011336 조민형 Week #12 HW-1

--- solveRangeInPerifocalFrame.m
+++ solveRangeInPerifocalFrame.m
@@ -1,3 +1,10 @@
+%--------------------------------------------------------------------------
+% Orbital Mechanics Week #12 HW-1
+% 20011336 조민형
+% Input: Semi-major axis (km), Eccentricity, True anomaly (deg)
+% Output: Range in Perifocal frame
+%--------------------------------------------------------------------------
+
 function rangeInPQW=solveRangeInPerifocalFrame(semimajor_axis,eccentricity,true_anomaly)
 
 a=semimajor_axis;
@@ -7,6 +14,5 @@ p=a*(1-e)^2;
 r=p/(1+e*cosd(nu));
 
 rangeInPQW=[r*cosd(nu);r*sind(nu);0];
-
 
 end
