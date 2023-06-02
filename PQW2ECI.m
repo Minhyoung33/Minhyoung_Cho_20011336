4a6f58d0ff33f0722eb475b614375f45119859a8
Minhyoung33 <85066478+Minhyoung33@users.noreply.github.com>
2023-06-02 15:45:07 +0900

    Update PQW2ECI.m
    Editted R2

--- PQW2ECI.m
+++ PQW2ECI.m
@@ -9,13 +9,15 @@ function mat_rot=PQW2ECI(arg_prg,inc_angle,RAAN)
 R1=[cosd(RAAN) sind(RAAN) 0;
     -sind(RAAN) cosd(RAAN) 0;
     0 0 1];
-R2=[cosd(inc_angle) 0 -sind(inc_angle);
-    0 1 0;
-    sind(inc_angle) 0 cosd(inc_angle)];
+R2=[1 0 0;
+    0 cosd(inc_angle) sind(inc_angle)
+    0 -sind(inc_angle) cosd(inc_angle)];
 R3=[cosd(arg_prg) sind(arg_prg) 0;
     -sind(arg_prg) cosd(arg_prg) 0;
     0 0 1];
 
 mat_rot=(R3*R2*R1)';
+
+
 
 end
