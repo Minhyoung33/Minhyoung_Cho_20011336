9938936187cbab61213fb58f3695395724c35ed5
jomin <jomin423@naver.com>
2023-06-02 16:11:06 +0900

    1611

--- solveRangeInPerifocalFrame.m
+++ solveRangeInPerifocalFrame.m
@@ -10,7 +10,7 @@ function rangeInPQW=solveRangeInPerifocalFrame(semimajor_axis,eccentricity,true_
 a=semimajor_axis;
 e=eccentricity; 
 nu=true_anomaly; %deg
-p=a*(1-e)^2;
+p=a*(1-e^2);
 r=p/(1+e*cosd(nu));
 
 rangeInPQW=[r*cosd(nu);r*sind(nu);0];
